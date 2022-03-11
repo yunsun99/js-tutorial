f6f99c587fd849922902ed4bb1d8bc0f41d3acc8
Yunsun Jung <yunsun992510@gmail.com>
2022-03-11 15:22:31 +0900

    first mission

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+import './styles.css';
+const colors = ['#1abc9c', '#3498db', '#9b59b6', '#f39c12', '#e74c3c'];
 
+const title = document.querySelector('h2');
+
+const superEventHandler = {
+  handleMouseEnter: function () {
+    title.innerText = 'The mouse is here!';
+    title.style.color = colors[0];
+  },
+  handleMouseLeave: function () {
+    title.innerText = 'The mouse is gone!';
+    title.style.color = colors[1];
+  },
+  handleWindowResize: function () {
+    title.innerText = 'You just resized!';
+    title.style.color = colors[2];
+  },
+  handleContextMenu: function () {
+    title.innerText = 'That was a right click!';
+    title.style.color = colors[4];
+  },
+};
+
+title.addEventListener('mouseenter', superEventHandler.handleMouseEnter);
+title.addEventListener('mouseleave', superEventHandler.handleMouseLeave);
+window.addEventListener('resize', superEventHandler.handleWindowResize);
+window.addEventListener('contextmenu', superEventHandler.handleContextMenu);
