a6567fd1e561ee21d66b7e7e20f40ea838a2bc35
Yunsun Jung <yunsun992510@gmail.com>
2022-03-16 00:42:00 +0900

    getting username

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-const colors = ['white', '#3498db', '#9b59b6', '#f39c12'];
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
 
-// 파랑 보라 노랑 순으로 커짐
-// colors[0] = 파랑
-// colors[1] = 보라
-// colors[2] = 노랑
+const HIDDEN_CLASSNAME = 'hidden';
 
-const body = document.querySelector('body');
-
-function handleWindowResize() {
-  let intViewportWidth = window.innerWidth;
-  if (intViewportWidth < 700) {
-    if (body.classList.contains('medium')) body.classList.remove('medium');
-    body.classList.add('small');
-  } else if (intViewportWidth < 1000) {
-    if (body.classList.contains('large')) body.classList.remove('large');
-    body.classList.add('medium');
-  } else {
-    body.classList.add('large');
-  }
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = 'Hello' + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-window.addEventListener('resize', handleWindowResize);
+loginForm.addEventListener('submit', onLoginSubmit);
